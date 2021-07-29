e49b868837d9f1785206f3ee9d2321e3417be03a
halitbatur <halitfuatbatur@gmail.com>
2021-07-30 01:54:42 +0300

    fix login problem with passportJS

--- site/util/passport-config.js
+++ site/util/passport-config.js
@@ -1,24 +1,18 @@
 const LocalStrategy = require("passport-local").Strategy;
-const User = require("../util/database");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 
 const intialize = (passport) => {
-    const validatePassword = async (password,userPassword) => {
-        console.log(password,userPassword)
-        return await bcrypt.compare(password,userPassword);
-    }
 
     passport.use(new LocalStrategy({usernameField:"email"},async (email,password,done) => {
-        try{
-            const user = User.findOne({where:{email}});
-            if(!user){return done(null,false)};
-            if(!validatePassword(password,user.password)){return done(null,false)};
-            return done(null,user);
-        }
-        catch(err){
-            return done(err);
-        }
+            return await User.findOne({where:{email}}).then(async (user) => {
+                if(!user){return done(null,false)};
+                return await bcrypt.compare(password,user.password,(err,res) => {
+                  if(!res){return done(null,false)}
+                  return done(null,user)
+                });
+             }).catch(err => done(err))
     }))
 }
 
