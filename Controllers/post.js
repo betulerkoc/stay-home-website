f2651ae43d96e94b010097a4182dddba81144b4d
betul <bet.er@hotmail.com>
2021-08-07 10:20:25 +0300

    update router

--- Controllers/post.js
+++ Controllers/post.js
@@ -1,4 +1,5 @@
 const PostDB = require("../models/PostDB");
+const { post } = require("../routes/router");
 
 exports.addPost = async (req, res, next) => {
   console.log(req.body);
@@ -7,12 +8,14 @@ exports.addPost = async (req, res, next) => {
   const number_of_days = req.body.number_of_days;
   const type_of_need = req.body.type_of_need;
   const location = req.body.location;
+  const isApplied = false;
   const newPost = PostDB.build({
     patient_id,
     number_of_family,
     number_of_days,
     type_of_need,
     location,
+    isApplied
   });
   try {
     await newPost.save();
@@ -23,10 +26,50 @@ exports.addPost = async (req, res, next) => {
 };
 
 exports.getPosts = async (req, res, next) => {
-  const posts = await PostDB.findAll();
-  console.log(req.body);
+  const posts = await PostDB.findAll({
+    where: {
+      isApplied: false
+    }
+  });
+  console.log(req.post);
   res.send(posts);
 };
+
+
+
+exports.getMyPosts = async (req, res, next) => {
+  const patientID = req.body.patientID
+  const myPosts = await PostDB.findAll({where: {
+    patient_id : patientID
+  }})
+  
+  res.send(myPosts)
+
+}
+
+
+exports.postVolunteerApplied = async (req, res, next) => {
+
+  const postID = req.body.postID
+
+  const post = await PostDB.findOne({where:{
+    annotation_id : postID
+  }})
+
+  //update the post row that is applied to true
+  await PostDB.update({isApplied: true}, {
+    where: {
+      annotation_id : postID
+    }
+  })
+
+  const postOwnerID = post.patient_id
+
+  res.send({
+    "postOwnerID": postOwnerID
+  })
+
+}
 
 exports.updatePost = async (req, res, next) => {
   const patient_id = req.body.patient_id;
--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 
-const PostDB = require("../models/PostDB");
-
 const usersControllers = require("../Controllers/users");
 const controllerPost = require("../Controllers/post");
 
@@ -20,33 +18,21 @@ router.post(
   usersControllers.postSignIn
 );
 
+//create new post
 router.post("/patient-post", controllerPost.addPost);
 
+//get all posts where isApplied = false
 router.get("/patient-post", controllerPost.getPosts);
+
+//get only posts of the patient signen in
+router.get("/getMyPosts", controllerPost.getMyPosts);
+
+router.post("/volunteer-applied", controllerPost.postVolunteerApplied);
 
 router.put("/patient-post/update", controllerPost.updatePost);
 
 router.delete("/patient-post/del/:id", controllerPost.deleteSpecificPost);
 
-// router.put("/patient-post/update", (req, res) => {
-//   const patient_id = req.body.patient_id;
-//   const number_of_family = req.body.number_of_family;
-//   const number_of_days = req.body.number_of_days;
-//   const type_of_need = req.body.type_of_need;
-//   const location = req.body.location;
-//   PostDB.update(
-//     {
-//       patient_id,
-//       number_of_family,
-//       number_of_days,
-//       type_of_need,
-//       location,
-//     },
-//     {
-//       where: { annotation_id: req.body.annotation_id }
-//     }
-//   ).then(() => res.send("success"));
-// });
 
 //exports
 module.exports = router;
